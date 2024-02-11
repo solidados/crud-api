2eda681bdab1f0e3cff6878cda75bc689b0485a6
solidados <geekbrains.pk@internet.ru>
2024-02-11 14:54:56 +0400

    chore: reconfigure all project plugins and settings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import { createServer } from 'http';
+
+createServer((_req, res): void => {
+  res.end('Hello!');
+}).listen(4000, (): void => {
+  console.log('Server is running on port: 4000');
+});
