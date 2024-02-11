7575ae87f47008a403d6bf67c3e62350183c9b75
solidados <geekbrains.pk@internet.ru>
2024-02-12 02:11:15 +0400

    chore: configure Application.ts using updated Middleware interface for CustomServerResponse

--- framework/Application.ts
+++ framework/Application.ts
@@ -1,56 +1,68 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 import EventEmitter from 'events';
 import Router from './Router';
+import { MiddlewareType } from './types/types';
+import { CustomServerResponse } from './Middleware';
 
 class Application {
   private readonly emitter: EventEmitter;
-  
+
   private readonly server: http.Server;
-  
+
   private router: Router;
-  
+
+  private middlewares: MiddlewareType[];
+
   constructor() {
     this.emitter = new EventEmitter();
     this.server = this.createServer();
     this.router = new Router();
+    this.middlewares = [];
   }
-  
-  private static getRouteMask( path: string, method: string ): string {
-    return `[${ path }]:[${ method }]`;
+
+  private static getRouteMask(path: string, method: string): string {
+    return `[${path}]:[${method}]`;
   }
-  
-  public listen( port: string, callback: ( error?: Error ) => void ): void {
-    this.server.listen( port, callback );
+
+  public use(middleware: MiddlewareType): void {
+    this.middlewares.push(middleware);
   }
-  
-  public addRouter( router: Router ): void {
+
+  public listen(port: string, callback: (error?: Error) => void): void {
+    this.server.listen(port, callback);
+  }
+
+  public addRouter(router: Router): void {
     this.router = router;
-    Object.keys( this.router.endpoints ).forEach( ( path: string ): void => {
-      const endpoint = this.router.endpoints[ path ];
-      
-      Object.keys( endpoint ).forEach( ( method: string ): void => {
-        const handler = endpoint[ method ];
-        
-        this.emitter.on( Application.getRouteMask( path, method ), ( req: IncomingMessage, res: ServerResponse ): void => {
-          handler( req, res );
-        } );
-      } );
-    } );
+    Object.keys(this.router.endpoints).forEach((path: string): void => {
+      const endpoint = this.router.endpoints[path];
+
+      Object.keys(endpoint).forEach((method: string): void => {
+        this.emitter.on(
+          Application.getRouteMask(path, method),
+          (req: IncomingMessage, res: CustomServerResponse): void => {
+            const handler = endpoint[method];
+            this.middlewares.forEach((middleware: MiddlewareType) => middleware(req, res));
+            handler(req, res);
+          },
+        );
+      });
+    });
   }
-  
+
   private createServer(): http.Server {
-    return http.createServer( ( req: IncomingMessage, res: ServerResponse ): void => {
+    return http.createServer((req: IncomingMessage, res: ServerResponse): void => {
       const emitted: boolean = this.emitter.emit(
-        Application.getRouteMask( req.url as string, req.method as string ),
+        Application.getRouteMask(req.url as string, req.method as string),
         req,
         res,
       );
-      
-      if ( !emitted ) {
+
+      if (!emitted) {
         res.statusCode = 404;
-        res.end( '404 Not Found' );
+        res.end('404 Not Found');
       }
-    } );
+    });
   }
 }
 
