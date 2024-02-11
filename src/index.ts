623e96731e1cf1143539638e1fd049792e9dadbc
solidados <geekbrains.pk@internet.ru>
2024-02-12 02:13:37 +0400

    refactor: index.ts/ delete comments; add PORT checks; use MiddlewareType un app.use(); correct types

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
 import 'dotenv/config';
 import Application from '../framework/Application';
 import UserRoutes from './users/user-routes';
+import { MiddlewareType } from '../framework/types/types';
+import Middleware from '../framework/Middleware';
 
 const { PORT } = process.env;
 
+if (!PORT) {
+  console.error('PORT is not defined in process.env.');
+  process.exit(1);
+}
+
 const app: Application = new Application();
+
+app.use(Middleware as MiddlewareType);
 
 app.addRouter(UserRoutes);
 
-app.listen(PORT as string, (error?: Error): void => {
+app.listen(PORT, (error?: Error): void => {
   if (error) {
     console.error(`Error starting server: ${error.message}`);
     process.exit(1);
   }
   console.log(`Server is running on port: ${PORT}`);
 });
-
-
-// import { ServerResponse, IncomingMessage } from 'http';
-// import Router from '../framework/Router';
-// const router: Router = new Router();
-// router.get(`${END_POINT}`, (_req: IncomingMessage, res: ServerResponse): void => {
-//   res.end(`YOUR REQUEST WAS SENT TO END POINT: ${END_POINT}`);
-// });
